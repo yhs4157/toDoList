8ad99d16e7694e147ccba7b4d888027e0bcc0e9c
yunhosung <yhs4157@naver.com>
2022-02-02 14:58:54 +0900

    add example at App to prop

--- src/App.js
+++ src/App.js
@@ -3,11 +3,29 @@ import TodoTemplate from './component/TodoTemplate';
 import TodoInsert from './component/TodoInsert';
 import TodoList from './component/TodoList';
 
+const [todos, setTodos] = useState([
+  {
+    id: 1, 
+    text: '리액트의 기초 알아보기',
+    check: true, 
+  },
+  {
+    id: 2, 
+    text: '컴포넌트 스타일링해 보기',
+    check: true, 
+  },
+  {
+    id: 3, 
+    text: '일정 관리 앱 만들어 보기',
+    check: false, 
+  },
+]);
+
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos = {todos}/>
     </TodoTemplate>
   );
 }
